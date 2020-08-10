4cc309214c48520e6d4966b26b864fcdf6289e32
randeath <66503063+randeath@users.noreply.github.com>
2020-08-10 19:17:18 +0900

    20/08/10

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.reservation  # 'dbsparta'라는 이름의 db를 만듭니다.
 
 
 ## HTML을 주는 부분
@@ -9,19 +13,44 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/client1')
+def client1():
+    return render_template('client1.html')
+
+
 ## API 역할을 하는 부분
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title_give')
-    print(title_receive)
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+@app.route('/push', methods=['POST'])
+def write_review():
+	# 1. 클라이언트가 준 title, author, review 가져오기.
+    title = request.form['title']
+    author = request.form['author']
+    review = request.form['review']
+    print("title", title)
+    print("author", author)
+    print("review", review)
 
 
-@app.route('/test', methods=['POST'])
-def test_post():
-    title_receive = request.form['title_give']
-    print(title_receive)
+
+	# 2. DB에 정보 삽입하기
+    newReview = {
+        'title': title,
+        'author': author,
+        'review': review
+    }
+    db.reviews.insert_one(newReview)
+
+	# 3. 성공 여부 & 성공 메시지 반환하기
     return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
+
+@app.route('/review', methods=['GET'])
+def read_reviews():
+    reviews = list(db.reviews.find({}, {'_id': 0}))
+    return jsonify({
+        'result': 'success',
+        'reviews': reviews
+    })
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
 
 if __name__ == '__main__':
